566cad31fd50357b012ac21a0f7cbf47c445703c
ahmedezzelareb7-cyber <ahmedezzelareb7@gmail.com>
2026-09-26 23:45:23 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='radsa-field-v4', FILES=['./','index.html','manifest.webmanifest','icon.svg','icon-192.png','icon-512.png'];
+const C='radsa-field-v6', FILES=['./','index.html','manifest.webmanifest','icon.svg','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;
